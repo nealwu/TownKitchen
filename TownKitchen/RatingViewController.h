aeb5c38591ed83eca8fb45a8c35fe984a8f8cd5e
Neal Wu <nwu@box.com>
2015-03-10 13:07:12 -0700

    Added RatingViewController

---
+++ TownKitchen/RatingViewController.h
@@ -0,0 +1,13 @@
+//
+//  RatingViewController.h
+//  TownKitchen
+//
+//  Created by Neal Wu on 3/9/15.
+//  Copyright (c) 2015 The Town Kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RatingViewController : UIViewController
+
+@end
---
+++ TownKitchen/RatingViewController.m
@@ -0,0 +1,119 @@
+//
+//  RatingViewController.m
+//  TownKitchen
+//
+//  Created by Neal Wu on 3/9/15.
+//  Copyright (c) 2015 The Town Kitchen. All rights reserved.
+//
+
+#import "RatingViewController.h"
+
+@interface RatingViewController () <UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *oneStarView;
+@property (weak, nonatomic) IBOutlet UIImageView *twoStarView;
+@property (weak, nonatomic) IBOutlet UIImageView *threeStarView;
+@property (weak, nonatomic) IBOutlet UIImageView *fourStarView;
+@property (weak, nonatomic) IBOutlet UIImageView *fiveStarView;
+@property (weak, nonatomic) IBOutlet UITextView *commentView;
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
+
+@property (assign, nonatomic) NSInteger ratingStars;
+
+@end
+
+@implementation RatingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.commentView.delegate = self;
+    self.commentView.textColor = [UIColor grayColor];
+    self.commentView.layer.borderWidth = 1;
+    self.commentView.layer.cornerRadius = 5;
+    self.submitButton.hidden = YES;
+    self.ratingStars = 0;
+}
+
+- (IBAction)onOneStarTap:(id)sender {
+    NSLog(@"One star tap");
+    self.oneStarView.image = [UIImage imageNamed:@"star-on"];
+    self.twoStarView.image = [UIImage imageNamed:@"star-off"];
+    self.threeStarView.image = [UIImage imageNamed:@"star-off"];
+    self.fourStarView.image = [UIImage imageNamed:@"star-off"];
+    self.fiveStarView.image = [UIImage imageNamed:@"star-off"];
+    self.submitButton.hidden = NO;
+    self.ratingStars = 1;
+}
+
+- (IBAction)onTwoStarTap:(id)sender {
+    NSLog(@"Two star tap");
+    self.oneStarView.image = [UIImage imageNamed:@"star-on"];
+    self.twoStarView.image = [UIImage imageNamed:@"star-on"];
+    self.threeStarView.image = [UIImage imageNamed:@"star-off"];
+    self.fourStarView.image = [UIImage imageNamed:@"star-off"];
+    self.fiveStarView.image = [UIImage imageNamed:@"star-off"];
+    self.submitButton.hidden = NO;
+    self.ratingStars = 2;
+
+}
+
+- (IBAction)onThreeStarTap:(id)sender {
+    NSLog(@"Three star tap");
+    self.oneStarView.image = [UIImage imageNamed:@"star-on"];
+    self.twoStarView.image = [UIImage imageNamed:@"star-on"];
+    self.threeStarView.image = [UIImage imageNamed:@"star-on"];
+    self.fourStarView.image = [UIImage imageNamed:@"star-off"];
+    self.fiveStarView.image = [UIImage imageNamed:@"star-off"];
+    self.submitButton.hidden = NO;
+    self.ratingStars = 3;
+}
+
+- (IBAction)onFourStarTap:(id)sender {
+    NSLog(@"Four star tap");
+    self.oneStarView.image = [UIImage imageNamed:@"star-on"];
+    self.twoStarView.image = [UIImage imageNamed:@"star-on"];
+    self.threeStarView.image = [UIImage imageNamed:@"star-on"];
+    self.fourStarView.image = [UIImage imageNamed:@"star-on"];
+    self.fiveStarView.image = [UIImage imageNamed:@"star-off"];
+    self.submitButton.hidden = NO;
+    self.ratingStars = 4;
+}
+
+- (IBAction)onFiveStarTap:(id)sender {
+    NSLog(@"Five star tap");
+    self.oneStarView.image = [UIImage imageNamed:@"star-on"];
+    self.twoStarView.image = [UIImage imageNamed:@"star-on"];
+    self.threeStarView.image = [UIImage imageNamed:@"star-on"];
+    self.fourStarView.image = [UIImage imageNamed:@"star-on"];
+    self.fiveStarView.image = [UIImage imageNamed:@"star-on"];
+    self.submitButton.hidden = NO;
+    self.ratingStars = 5;
+}
+
+- (BOOL)textViewShouldBeginEditing:(UITextView *)textView {
+    if ([textView.text isEqual:@"Leave a review here (optional)"]) {
+        textView.text = @"";
+        textView.textColor = [UIColor blackColor];
+    }
+
+    return YES;
+}
+
+- (BOOL)textViewShouldEndEditing:(UITextView *)textView {
+    if (textView.text.length == 0) {
+        textView.text = @"Leave a review here (optional)";
+        textView.textColor = [UIColor grayColor];
+    }
+
+    return YES;
+}
+
+- (IBAction)onGeneralTap:(id)sender {
+    [self.commentView endEditing:YES];
+}
+
+- (IBAction)onSubmit:(id)sender {
+    NSLog(@"Submitted with %ld star(s) and comment %@", self.ratingStars, self.commentView.text);
+}
+
+@end
