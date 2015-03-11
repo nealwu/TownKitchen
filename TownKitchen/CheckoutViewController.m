d9038b80ec7e03604a9556974710197260ed8e41
Peter Bai <peter@peterbai.com>
2015-03-11 06:46:09 -0700

    sort order.items

--- TownKitchen/CheckoutViewController.m
+++ TownKitchen/CheckoutViewController.m
@@ -32,7 +32,25 @@
 
 - (void)setOrder:(Order *)order {
     _order = order;
+    NSMutableArray *mutableOrderItems = [NSMutableArray array];
+    for (NSString *menuItem in order.items) {
+        [mutableOrderItems addObject:@{ @"menuItem" : menuItem, @"quantity" : order.items[menuItem] }];
+    }
+    NSLog(@"%@", mutableOrderItems);
     
+    [mutableOrderItems sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        if (obj1[@"quantity"] > obj2[@"quantity"]) {
+            return NSOrderedAscending;
+        }
+        else if (obj1[@"quantity"] < obj2[@"quantity"]) {
+            return NSOrderedDescending;
+        }
+        else {
+            return NSOrderedSame;
+        }
+    }];
+
+    NSLog(@"%@", mutableOrderItems);
 }
 
 #pragma mark Private Methods
@@ -42,6 +60,43 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     [self.tableView registerNib:[UINib nibWithNibName:@"CheckoutOrderItemCell" bundle:nil] forCellReuseIdentifier:@"CheckoutOrderItemCell"];
+    
+    // retrieve order objects from Parse
+    
+    for (NSString *orderItem in self.order.items) {
+        
+    }
+    
+    PFQuery *orderQuery = [Order query];
+    
+
+    
+    /* PFQuery *inventoryQuery = [Inventory query];
+    [inventoryQuery whereKey:@"dateOffered" greaterThanOrEqualTo:dateStart];
+    [inventoryQuery whereKey:@"dateOffered" lessThanOrEqualTo:dateEnd];
+    [inventoryQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (error) {
+            NSLog(@"failed to find inventory objects, error: %@", error);
+            return;
+        }
+        self.dayInventory = [[DayInventory alloc] init];
+        self.dayInventory.inventoryItems = objects;
+        NSLog(@"Today's Inventory: %@", self.dayInventory.inventoryItems);
+        
+        // Retrieve corresponding menu options
+        
+        for (Inventory *inventoryItem in self.dayInventory.inventoryItems) {
+            PFQuery *menuOptionQuery = [MenuOption query];
+            [menuOptionQuery whereKey:@"name" equalTo:inventoryItem.menuOption];
+            [menuOptionQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+                if (error) {
+                    NSLog(@"failed to find menu option, error: %@", error);
+                }
+                inventoryItem.menuOptionObject = [objects firstObject];
+                [self reloadAllTableViewData];
+            }];
+        }
+    }]; */
     
 }
 #pragma mark Table view methods
