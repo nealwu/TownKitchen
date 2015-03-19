26d507db42aabaeb502036ff1317af28d98d0c54
Peter Bai <peter@peterbai.com>
2015-03-18 19:38:09 -0700

    custom navigationbar class

---
+++ TownKitchen/TKNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  TKNavigationBar.h
+//  TownKitchen
+//
+//  Created by Peter Bai on 3/18/15.
+//  Copyright (c) 2015 The Town Kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKNavigationBar : UINavigationBar
+
+@end
---
+++ TownKitchen/TKNavigationBar.m
@@ -0,0 +1,28 @@
+//
+//  TKNavigationBar.m
+//  TownKitchen
+//
+//  Created by Peter Bai on 3/18/15.
+//  Copyright (c) 2015 The Town Kitchen. All rights reserved.
+//
+
+#import "TKNavigationBar.h"
+
+@interface TKNavigationBar ()
+
+
+
+@end
+
+@implementation TKNavigationBar
+
+- (void)layoutSubviews {
+    self.translucent = NO;
+    self.barTintColor = [UIColor colorWithRed:235.0 / 255
+                                        green:92.0 / 255
+                                         blue:87.0 / 255
+                                        alpha:1.0];
+    
+}
+
+@end
