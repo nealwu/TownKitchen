c3d92ab1a7bf1b16dcb95d59f40d060d459633b7
Peter Bai <peter@peterbai.com>
2015-03-18 21:49:51 -0700

    template for checkoutView

---
+++ TownKitchen/CheckoutView.h
@@ -0,0 +1,13 @@
+//
+//  CheckoutView.h
+//  TownKitchen
+//
+//  Created by Peter Bai on 3/18/15.
+//  Copyright (c) 2015 The Town Kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckoutView : UIView
+
+@end
---
+++ TownKitchen/CheckoutView.m
@@ -0,0 +1,43 @@
+//
+//  CheckoutView.m
+//  TownKitchen
+//
+//  Created by Peter Bai on 3/18/15.
+//  Copyright (c) 2015 The Town Kitchen. All rights reserved.
+//
+
+#import "CheckoutView.h"
+
+@interface CheckoutView ()
+
+@property (strong, nonatomic) IBOutlet UIView *contentView;
+
+@end
+
+@implementation CheckoutView
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self initSubViews];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self initSubViews];
+    }
+    return self;
+}
+
+- (void)initSubViews {
+    UINib *nib = [UINib nibWithNibName:@"CircleStepperView" bundle:nil];
+    [nib instantiateWithOwner:self options:nil];
+    self.contentView.frame = self.bounds;
+    [self addSubview:self.contentView];
+}
+
+
+@end
