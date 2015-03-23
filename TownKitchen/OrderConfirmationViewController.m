38495b3963f59ca12f7c800ff2f3de755e920eac
Peter Bai <peter@peterbai.com>
2015-03-22 17:39:45 -0700

    new files for orderconfirmationview

---
+++ TownKitchen/OrderConfirmationViewController.m
@@ -0,0 +1,37 @@
+//
+//  OrderConfirmationViewController.m
+//  TownKitchen
+//
+//  Created by Peter Bai on 3/22/15.
+//  Copyright (c) 2015 The Town Kitchen. All rights reserved.
+//
+
+#import "OrderConfirmationViewController.h"
+
+@interface OrderConfirmationViewController ()
+
+@end
+
+@implementation OrderConfirmationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
