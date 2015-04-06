9cfba450e55fd385dd90d5509b13c673f288a09b
Peter Bai <peter@peterbai.com>
2015-04-05 17:49:34 -0700

    paymentviewcontroller

---
+++ TownKitchen/PaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaymentViewController.h
+//  TownKitchen
+//
+//  Created by Peter Bai on 4/5/15.
+//  Copyright (c) 2015 The Town Kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentViewController : UIViewController
+
+@end
